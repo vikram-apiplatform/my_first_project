a8b1dd98a86b9dc5316f949e5c327c1d3ef48e71
dev-vikram <vikram.s@apiplatform.io>
2022-05-12 12:48:05 +0530

    commit printTestData BUSINESS_LOGIC_API

---
+++ dev-vikram/dev-vikram/BUSINESS_LOGIC_API/printTestData_v1.py
@@ -0,0 +1,14 @@
+import requests
+
+name = None
+i = None
+
+
+def business_logic_api():
+  name = 'vikram'
+
+  API_gettest_v1 = requests.get(url = 'https://dev-vikram.gateway.apiplatform.io/v1/test',
+    headers = {'pkey' : '3fbbb8bc5a969f503fdb66e7d90509d6', 'apikey' : '7xR2sYhqRAdfhhyu6jMo9E9hi4fRazuw'},
+    params = {'query' : ''})
+  for i in (API_gettest_v1.json()):
+    print(i)
