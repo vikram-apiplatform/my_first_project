29d56a13ceb9da19d7b7d67af85cd7659b962874
dev-vikram <vikram.s@apiplatform.io>
2022-06-23 11:02:15 +0530

    git commit

---
+++ Product/src/test/java/io/apiplatform/v1/Product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package io.apiplatform.v1.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
